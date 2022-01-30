e59210c35405c2bd1e3ce08c3427049c58ac278f
Tanishk Goyal <goyaltanishk02@gmail.com>
2022-01-30 13:43:45 +0530

    deployment complete

--- contracts/TheDot.sol
+++ contracts/TheDot.sol
@@ -7,11 +7,11 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
+contract TheDot is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() public ERC721("the.dot", "DOT") {}
+    constructor() public ERC721("TheDot", "DOT") {}
 
     function mintNFT(address recipient, string memory tokenURI)
         public
