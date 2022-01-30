e1284609dfa18821b55a7bce38b0521d11a0722a
Tanishk Goyal <goyaltanishk02@gmail.com>
2022-01-30 19:26:00 +0530

    metadata complete

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,7 +1,22 @@
 require("dotenv").config()
 const API_URL = process.env.API_URL
+const PUBLIC_KEY = process.env.PUBLIC_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
 const web3 = createAlchemyWeb3(API_URL)
 const contract = require("../artifacts/contracts/TheDot.sol/TheDot.json")
 console.log(JSON.stringify(contract.abi))
+const contractAddress = "0x88287dbdfddd6bfe0eb3bb4949ac6ebb91d2f88b"
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
 
+async function mintNFT(tokenURI) {
+    const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest')
+    const tx = {
+        'from': PUBLIC_KEY,
+        'to': contractAddress,
+        'nonce': nonce,
+        'gas': 500000,
+        'data': nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI()
+    };
+}
