432fd41679d515c0aa8cbfc73294753a56a90b60
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-01-31 19:23:26 +0530

    Metamask done

---
+++ frontend/src/App.js
@@ -0,0 +1,93 @@
+import React, {useEffect, useState} from "react";
+import './App.css';
+import MetaMaskOnboarding from '@metamask/onboarding';
+
+const checkIfWalletIsConnect = async (setCurrentAccount) => {
+    try {
+        if (!window.ethereum) return; // alert("Please install MetaMask.");
+
+        const accounts = await window.ethereum.request({method: "eth_accounts"});
+        console.log("CHECK IF WALLERY IS CONNNNNNNNN");
+        if (accounts.length) {
+            setCurrentAccount(accounts[0]);
+        } else {
+            console.log("No accounts found");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const connectWallet = async (setCurrentAccount) => {
+    try {
+        if (!window.ethereum) {
+            const onboarding = new MetaMaskOnboarding({
+                forwarderOrigin: "https://macaroon.web.app",
+            });
+            onboarding.startOnboarding();
+        }
+
+        const accounts = await window.ethereum.request({method: "eth_requestAccounts",});
+
+        setCurrentAccount(accounts[0]);
+    } catch (error) {
+        console.log(error);
+
+        throw new Error("No window.ethereum object");
+    }
+};
+
+function isActualOwner(accountAddress) {
+    console.log("CURRENT ACCOUNT", accountAddress);
+    return true;
+}
+async function isValidOwner(currentAccount, setCurrentAccount) {
+    let account = currentAccount;
+    if (!account) {
+        account = await connectWallet(setCurrentAccount);
+    }
+    if (account === undefined) {
+        return ".";
+    }
+    if (isActualOwner(account)) {
+        return account;
+    }
+    return "";
+}
+function App() {
+    const [clicked, setClicked] = useState(false);
+    const [text, setText] = useState("");
+    const [currentAccount, setCurrentAccount] = useState("");
+
+    useEffect(() => {
+        async function f(){
+
+            const owner = await isValidOwner(currentAccount, setCurrentAccount);
+            if (owner) {
+                setText(owner)
+            } else {
+                window.location.href = "https://www.google.com";
+            }
+        }
+
+        if (clicked && !text)
+            f();
+    }, [clicked, currentAccount, text]);
+  return (
+    <div className="App">
+      <header className="App-header">
+          <div className="">
+        {/*<img src={logo} className="App-real-logo" alt="logo" onClick={()=>setClicked(true)}/>*/}
+          <div className='circle blue App-logo' onClick={() => {
+              setClicked(true);
+              // alert("clicked");
+          }}>
+              <p style={{wordWrap: "break-word", width: "200px", margin: "auto"}}> {text} </p>
+          </div>
+          </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
